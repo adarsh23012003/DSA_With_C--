b7ac1a74c5e178932fc7b818eae10dd9219e7168
Adarsh Maurya <adarshmaurya2303@gmail.com>
2026-07-01 11:24:13 +0530

    Update tringal.cpp to print numbers instead of characters and remove unused variable

--- Patterns/tringal.cpp
+++ Patterns/tringal.cpp
@@ -3,15 +3,13 @@ using namespace std;
 int main()
 {
     int n = 5;
-    char ch = 'A';
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
             // cout << "* ";
-            cout << ch << " ";
+            cout << j << " ";
         }
-        ch = ch + 1;
         cout << endl;
     }
     return 0;
