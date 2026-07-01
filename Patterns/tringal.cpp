65b3e49e1c051f97c7e3822f7f4c6e402d146e1e
Adarsh Maurya <adarshmaurya2303@gmail.com>
2026-07-01 11:22:09 +0530

    Update tringal.cpp to print characters instead of numbers and adjust pattern size

--- Patterns/tringal.cpp
+++ Patterns/tringal.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main()
 {
-    int n = 4;
+    int n = 5;
+    char ch = 'A';
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
             // cout << "* ";
-            cout << i << " ";
+            cout << ch << " ";
         }
+        ch = ch + 1;
         cout << endl;
     }
     return 0;
