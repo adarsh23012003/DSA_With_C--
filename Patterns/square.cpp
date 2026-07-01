d8afe39055eb94507c6451d77bb4e95b2b8a6030
Adarsh Maurya <adarshmaurya2303@gmail.com>
2026-07-01 10:53:39 +0530

    Refactor square.cpp to remove redundant variable declaration and clean up commented code

--- Patterns/square.cpp
+++ Patterns/square.cpp
@@ -4,20 +4,25 @@ int main()
 {
     int n = 3;
     int num = 1;
+    char ch = 'A';
     for (int i = 1; i <= n; i++)
     {
-        char ch = 'A';
+        // char ch = 'A';
         for (int j = 1; j <= n; j++)
         {
             // cout << j << " ";
 
-            cout << "* ";
+            // cout << "* ";
 
             // cout << ch << " ";
             // ch++;
 
             // cout << num << " ";
             // num++;
+
+            // Homework:
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
